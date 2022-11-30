f7c8c718171921847a57007bc4b39bd41838ed5c
hasnafar <hasnafarrukh@gmail.com>
2022-11-30 14:10:27 -0500

    set up react app

---
+++ src/components/Project.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+
+function Project() {
+    return (
+        <section class="header">
+
+        {/* Developer Name */}
+            <h1> Hasna Farrukh   </h1>
+
+            {/* Page Navigation */}
+            <nav>{Navigation}</nav>
+        </section>
+    )
+}
+
+export default Project;
